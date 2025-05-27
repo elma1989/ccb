02cabf4433a6e43c8299e4ffb5f529b35d5124e5
Marco Elste <marco.elste@web.de>
2025-05-27 20:36:26 +0200

    Added database/country.py

---
+++ src/database/country.py
@@ -0,0 +1,40 @@
+from database import DataObject, Error
+
+class Country(DataObject):
+    """
+    Repräsentiert ein Land.
+
+    :param cs: Kfz-Kennzeichen
+    :param name: Name des Landes (Standard: "")
+    """
+    def __init__(self, cs:str, name:str = "") -> None:
+        self.__cs = cs.upper()
+        self.__name = name
+
+    @property
+    def cs(self) -> str:
+        ''':getter: Kfz-Kennzeichen'''
+        return self.__cs
+    
+    @property
+    def name(self) -> str:
+        ''':getter: Landname'''
+        sql:str = 'SELECT cty_name FROM country WHERE cty_cs = ?'
+
+        if len(self.__name) == 0:
+            try:
+                self.connect()
+                if self.con and self.c:
+                    self.c.execute(sql,(self.cs,))
+                    res = self.c.fetchone()
+                    if res: self.__name = res[0]
+            except Error as e: print(e)
+            finally: self.close()
+
+        return self.__name
+    
+    def __repr__(self) -> str: return self.cs
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Country): return False
+        return self.cs == other.cs
